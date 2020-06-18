b46fd9b92fd6701427e97d14c7d5e634eb85a162
Maxime Deroullers <mderoullers@ippon.fr>
2020-06-18 18:42:40 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-58f6046995facb2b5c6f.js"
+    "url": "webpack-runtime-40f68ca7b25a4effbbfa.js"
   },
   {
     "url": "framework-dd95e8b3d258d85252e9.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5b04c22fd79ec4a318c2e6401b9b15a1"
+    "revision": "4205dcd22f7128a865e40bd6b6ee3763"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-235e751eb037707e5d1a.js"
