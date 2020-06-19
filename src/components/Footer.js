bf4e5adfb94e851aa00a9f606580ea0037ce9455
Maxime Deroullers <mderoullers@ippon.fr>
2020-06-19 11:30:22 +0200

    add copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ class Footer extends React.Component {
                     <li><a href="#" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
                 </ul>
                 <ul className="copyright">
-                    <li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                    <li>&copy; 2020</li><li>Fait avec <i className="icon alt fa-heart"></i> à Lille par la team Cloud Nord</li>
                 </ul>
             </section>
         )
