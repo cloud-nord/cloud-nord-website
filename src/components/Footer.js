3819083d79e7284e678315cfdadaf07b98de137a
Maxime Deroullers <mderoullers@ippon.fr>
2020-06-19 10:08:37 +0200

    add linkedin logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,8 @@ class Footer extends React.Component {
         return (
             <section id="footer">
                 <ul className="icons">
+                    <li><a href="#" className="icon alt fa-linkedin"><span className="label">Linkedin</span></a></li>
                     <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                    <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                    <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
                     <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
                     <li><a href="#" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
                 </ul>
