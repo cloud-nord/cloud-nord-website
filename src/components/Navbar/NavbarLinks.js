0e10e301c81ecbcc46592a60050daf0eba273429
Maxime Deroullers <mderoullers@ippon.fr>
2020-06-23 19:17:53 +0200

    fix color of navbar links

--- src/components/Navbar/NavbarLinks.js
+++ src/components/Navbar/NavbarLinks.js
@@ -19,13 +19,13 @@ const NavItem = styled(Link)`
     width: 0%;
     content: ".";
     color: transparent;
-    background: goldenrod;
+    background: #4a87d3;
     height: 1px;
     transition: all 0.4s ease-in;
   }
 
   :hover {
-    color: goldenrod;
+    color: #4a87d3;
     ::after {
       width: 100%;
     }
