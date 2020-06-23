42af10e1232c06e922408eaf46fb31836754888c
Maxime Deroullers <mderoullers@ippon.fr>
2020-06-23 19:17:18 +0200

    fix logo size

--- src/components/Navbar/Logo.js
+++ src/components/Navbar/Logo.js
@@ -4,12 +4,12 @@ import Img from "gatsby-image"
 import { Link, useStaticQuery, graphql } from "gatsby"
 
 const LogoWrap = styled.div`
-  margin: auto 0;
-  flex: 0 1 36px;
-
-  @media (max-width: 768px) and (orientation: landscape) {
-    flex: 0 1 25px;
-  }
+    margin: auto 0;
+    flex: 0 1 100px;
+    
+    @media (max-width: 768px) and (orientation: landscape) {
+        flex: 0 1 25px;
+    } 
 `
 const Logo = () => {
     const data = useStaticQuery(graphql`
