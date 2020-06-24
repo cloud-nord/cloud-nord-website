7aa4a419d658bc2cedac85eb988ab131d9f5fbc2
Maxime Deroullers <mderoullers@ippon.fr>
2020-06-24 16:50:20 +0200

    fix footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ class Footer extends React.Component {
         return (
             <section id="footer">
                 <ul className="icons">
-                    <li><a href="#" className="icon alt fa-linkedin"><span className="label">Linkedin</span></a></li>
+                    <li><a href="https://www.linkedin.com/company/cloud-nord/" className="icon alt fa-linkedin"><span className="label">Linkedin</span></a></li>
                     <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                    <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                    <li><a href="#" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
+                    <li><a href="https://github.com/cloud-nord" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+                    <li><a href="mailto:cloudnord2020@gmail.com" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
                 </ul>
                 <ul className="copyright">
                     <li>&copy; 2020</li><li>Fait avec <i className="icon alt fa-heart"></i> à Lille par la team Cloud Nord</li>
