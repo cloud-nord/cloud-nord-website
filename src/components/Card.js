e91d8139d36630a2d8ae5ebaa3e1369da35a2f75
Maxime Deroullers <mderoullers@ippon.fr>
2020-06-29 11:32:45 +0200

    format code

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,40 +1,39 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import styled from "styled-components"
+import React from "react";
+import { graphql } from "gatsby";
+import styled from "styled-components";
 
 // ========================
 // The primary card container
 const CardWrap = styled.div`
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-    transition: 0.3s;
-`
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+`;
 // ========================
 
 // ========================
 // The card header
 const CardHeader = styled.h1`
-    font-weight: bold;
-    text-align: center;;
-`
+  font-weight: bold;
+  text-align: center;
+`;
 // ========================
 
 // ========================
 // Primary copy (inc. links, etc)
-const CardCopy = styled.div`
-`
+const CardCopy = styled.div``;
 // ========================
 
 class Card extends React.Component {
-    render() {
-        return (
-            <CardWrap>
-                <CardHeader>{this.props.title}</CardHeader>
-                <CardCopy>
-                    <div dangerouslySetInnerHTML={{__html: this.props.content}}/>
-                </CardCopy>
-            </CardWrap>
-        )
-    }
+  render() {
+    return (
+      <CardWrap>
+        <CardHeader>{this.props.title}</CardHeader>
+        <CardCopy>
+          <div dangerouslySetInnerHTML={{ __html: this.props.content }} />
+        </CardCopy>
+      </CardWrap>
+    );
+  }
 }
 
 export default Card;
