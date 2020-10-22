227abddaabf33c505cfc6f25b0678000f81bd2fe
Maxime Deroullers <maxime.deroullers@gmail.com>
2020-10-22 23:14:38 +0200

    feat: add modal and change agenda design

--- src/components/Schedule/Talk.js
+++ src/components/Schedule/Talk.js
@@ -1,27 +1,58 @@
 import React from "react";
+import Modal from 'react-modal';
 
 class Talk extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showModal: false
+        };
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
+
     static defaultProps = {
         colSize: 1,
         rowSize: 1,
-        color: "stage-important",
+        color: "stage-default",
         room: "Salle 1",
         duration: "45 min",
         category: "Orga 2020",
         speaker: "",
-        company: ""
+        company: "",
+        badgesColor: "badge-color",
+        description: ""
+    }
+
+    handleOpenModal () {
+        this.setState({ showModal: true });
+    }
+
+    handleCloseModal () {
+        this.setState({ showModal: false });
     }
 
     render() {
+        const customStylesModal = {
+            content : {
+                top                   : '50%',
+                left                  : '50%',
+                right                 : 'auto',
+                bottom                : 'auto',
+                marginRight           : '-50%',
+                transform             : 'translate(-50%, -50%)'
+            }
+        };
         const speaker = this.props.speaker
         const company = this.props.company
         let displaySubjectSpeaker = <p><strong>{this.props.subject}</strong></p>; 
         if (speaker.length > 0) {
             if(company.length > 0){
-                displaySubjectSpeaker = <p><strong>{this.props.subject}</strong><br/>{speaker} {company}</p>;
+                displaySubjectSpeaker = <p><a onClick={this.handleOpenModal}><strong>{this.props.subject}</strong></a><br/>{speaker} {company}</p>;
             }
             else
-                displaySubjectSpeaker = <p><strong>{this.props.subject}</strong><br/>{speaker}</p>;
+                displaySubjectSpeaker = <p><a onClick={this.handleOpenModal}><strong>{this.props.subject}</strong></a><br/>{speaker}</p>;
         }
         return (
             <td
@@ -30,7 +61,24 @@ class Talk extends React.Component {
                 className={this.props.color}
             >
                 {displaySubjectSpeaker}
-                <div>
+                <Modal
+                    isOpen={this.state.showModal}
+                    contentLabel="Minimal Modal Example"
+                    style={customStylesModal}
+                >
+                    <section className="main style1">
+                        <div className="grid-wrapper">
+                            <div className="col-12">
+                                <header className="major">
+                                    <h3>{this.props.subject}</h3>
+                                </header>
+                                <p>{this.props.description}</p>
+                            </div>
+                        </div>
+                        <a className="button" onClick={this.handleCloseModal}>Close</a>
+                    </section>
+                </Modal>
+                <div className={this.props.badgesColor}>
                     <span>{this.props.duration}</span>
                     <span>{this.props.category}</span>
                     <span>{this.props.room}</span>
--- src/pages/agenda.js
+++ src/pages/agenda.js
@@ -15,32 +15,30 @@ class Agenda extends React.Component {
                                 <tbody>
                                 <tr>
                                     <Slot hour="09:00"/>
-                                    <Talk colSize="4" color="stage-orga" subject="Café avec l'orga" duration="15min"
+                                    <Talk colSize="4" color="stage-orga" subject="Café avec l'orga ☕" duration="15min"
                                           room="Salle 1"/>
                                 </tr>
                                 <tr>
                                     <Slot hour="09:15"/>
-                                    <Talk colSize="4" color="stage-keynote" subject="Keynote" duration="30min"
+                                    <Talk colSize="4" color="stage-orga" subject="Keynote" duration="30min"
                                           room="Salle 1"/>
                                 </tr>
                                 <tr>
                                     <Slot hour="09:45"/>
-                                    <Talk color="stage-conf"
-                                          subject="Et si ca tourne mal ? Recettes pour des applications résilientes"
+                                    <Talk subject="Et si ca tourne mal ? Recettes pour des applications résilientes"
                                           speaker="Christophe Maneu"
                                           company = "@Microsoft"
                                           duration="45min" room="Salle 1" category="Cloud Native"/>
-                                    <Talk color="stage-conf"
-                                          subject="FinOps : et si on arrêtait de balancer l'argent par les fenêtres ?"
+                                    <Talk subject="FinOps : et si on arrêtait de balancer l'argent par les fenêtres ?"
                                           speaker="Laurent Grangeau & Olivier Dupré & Tony Jarriault"
                                           company="@Sogeti"
-                                          duration="45min" room="Salle 2" category="DevOps & SRE"/>
-                                    <Talk color="stage-conf"
-                                          subject="Les bonnes pratiques de sécurité pour les applications Serverless"
+                                          duration="45min" room="Salle 2" category="DevOps & SRE"
+                                          description="Consommer les services d'un clouder est très simple. Payer beaucoup trop cher aussi. Malgré la promesse d'un cloud à faible coût, il faut avoir une attitude FinOps si on en veut pas voir la facture exploser. Notre objectif est de vous proposer des pistes d'optimisations des coûts sur vos clouds, publiques comme privés. On vous présentera une approche organisationnelle avant de mettre les mains dans le code et vous montrer 2 cas très concrets sur GCP et AWS."/>
+                                    <Talk subject="Les bonnes pratiques de sécurité pour les applications Serverless"
                                           speaker="Steve Houel"
                                           company="@AWS"
                                           duration="45min" room="Salle 3" category="Observabilité"/>
-                                    <Talk rowSize="3" color="stage-deepdive"
+                                    <Talk rowSize="3"
                                           subject="Istio ⛵ par la pratique !"
                                           speaker="Kevin Davin & Nikola Lohinski"
                                           company="@Stack Labs"
@@ -52,72 +50,61 @@ class Agenda extends React.Component {
                                 </tr>
                                 <tr>
                                     <Slot hour="10:55"/>
-                                    <Talk color="stage-conf"
-                                          subject="9 questions que vous devriez vous poser sur vos applications Serverless"
+                                    <Talk subject="9 questions que vous devriez vous poser sur vos applications Serverless"
                                           speaker="François Bouteruche"
                                           company="@AWS"
                                           duration="45min" room="Salle 1" category="Cloud Native"/>
-                                    <Talk color="stage-conf"
-                                          subject="Du DevOps multi Cloud avec Kubernetes et Terraform"
+                                    <Talk subject="Du DevOps multi Cloud avec Kubernetes et Terraform"
                                           speaker="Thomas Rannou"
                                           company="@Sopra Steria"
                                           duration="45min" room="Salle 2" category="DevOps & SRE"/>
-                                    <Talk color="stage-conf"
-                                          subject="REX : Comment créer une solution de log management à l’aide de Datadog ?"
+                                    <Talk subject="REX : Comment créer une solution de log management à l’aide de Datadog ?"
                                           speaker="Anthony Durot & Xavier Top"
                                           company="@Ippon & @Nexity"
                                           duration="45min" room="Salle 3" category="REX"/>
                                 </tr>
                                 <tr>
                                     <Slot hour="11:50"/>
-                                    <Talk color="stage-qwickie"
-                                          subject="Introduction à AWS : son Histoire, son Leadership, ses Clients, ses Services"
+                                    <Talk subject="Introduction à AWS : son Histoire, son Leadership, ses Clients, ses Services"
                                           speaker="Olivier Lemaitre"
                                           duration="15min" room="Salle 1" category="Cloud Native"/>
-                                    <Talk color="stage-qwickie"
-                                          subject="Kubernetes enfin ultra simple & léger avec K3S"
+                                    <Talk subject="Kubernetes enfin ultra simple & léger avec K3S"
                                           speaker="Sébastien Moreno"
                                           company="@Ippon"
-                                          duration="15min" room="Salle 2" category="Orchestration & Conteneurs"/>
-                                    <Talk color="stage-qwickie"
-                                          subject="Cloud Souverain, par où commencer ?"
+                                          duration="15min" room="Salle 2" category="Orchestration"/>
+                                    <Talk subject="Cloud Souverain, par où commencer ?"
                                           speaker="Christophe Chaudier"
                                           company="@Youtube 'Les Compagnons du DevOps'"
                                           duration="15min" room="Salle 3" category="Cloud Native"/>
-                                    <Talk color="stage-qwickie"
-                                          subject="Démystifier GitOps"
+                                    <Talk subject="Démystifier GitOps"
                                           speaker="Mohamed Chaaben"
                                           company="@Revolve"
                                           duration="15min" room="Salle 4" category="DevOps & SRE"/>
                                 </tr>
                                 <tr>
                                     <Slot hour="12:15"/>
-                                    <Talk colSize="4" color="stage-accent6" 
-                                          subject="Cloud Guru, DevSecOps, SRE, SysOps, Cloud Builder, DevOps, Triceratops…. Finalement c’est quoi les métiers du cloud en 2020 ? L’occasion de comprendre ce que sont les métiers du Cloud en 2020, comment recruter, faire évoluer et animer ses équipes pour construire son organisation de demain" 
+                                    <Talk colSize="4" color="stage-orga"
+                                          subject="Cloud Guru, DevSecOps, SRE, SysOps, Cloud Builder, DevOps, Triceratops…. Finalement c’est quoi les métiers du cloud en 2020 ? L’occasion de comprendre ce que sont les métiers du Cloud en 2020, comment recruter, faire évoluer et animer ses équipes pour construire son organisation de demain"
                                           duration="1h30"
                                           category="Table ronde"
                                           speaker="We Love Devs"
-                                          company=""
                                           room="Salle 1"/>
                                 </tr>
                                 <tr>
                                     <Slot hour="13:45"/>
-                                    <Talk color="stage-conf"
-                                          subject="Les opérateurs Kubernetes : opérer des services Cloud Native à grande échelle"
+                                    <Talk subject="Les opérateurs Kubernetes : opérer des services Cloud Native à grande échelle"
                                           speaker="Horacio Gonzalez"
                                           company = "@OVH"
                                           duration="45min" room="Salle 1" category="Cloud Native"/>
-                                    <Talk color="stage-conf"
-                                          subject="Rex : Datadog from Rake to CNAB, an history of deploy tools"
+                                    <Talk subject="Rex : Datadog from Rake to CNAB, an history of deploy tools"
                                           speaker="Pierre Baillet"
                                           company="@Datadog"
                                           duration="45min" room="Salle 2" category="REX"/>
-                                    <Talk color="stage-conf"
-                                          subject="Rex : Tencent Multicloud performant entre Europe ET Asie: notre XP!"
+                                    <Talk subject="Rex : Tencent Multicloud performant entre Europe ET Asie: notre XP!"
                                           speaker="Gilles Closset & Tudor Toma"
                                           company="@Tencent Cloud"
                                           duration="45min" room="Salle 3" category="REX"/>
-                                    <Talk rowSize="3" color="stage-deepdive"
+                                    <Talk rowSize="3"
                                           subject="Hands On : Les mains dans le serverless de Google Cloud"
                                           speaker="Guillaume Laforge"
                                           company="@Google"
@@ -125,68 +112,57 @@ class Agenda extends React.Component {
                                 </tr>
                                 <tr>
                                     <Slot hour="14:40"/>
-                                    <Talk color="stage-conf"
-                                          subject="Créer une application Cloud Native avec Quarkus et Google Cloud Functions"
+                                    <Talk subject="Créer une application Cloud Native avec Quarkus et Google Cloud Functions"
                                           speaker="Loïc Mathieu"
                                           company="@Zenika"
                                           duration="45min" room="Salle 1" category="Cloud Native"/>
-                                    <Talk color="stage-conf"
-                                          subject="SRE, où comment on laisse la Prod voyager de ses propres ailes"
+                                    <Talk subject="SRE, où comment on laisse la Prod voyager de ses propres ailes"
                                           speaker="Sylvain Nieuwlandt"
                                           company="@Sfeir"
                                           duration="45min" room="Salle 2" category="DevOps & SRE"/>
-                                    <Talk color="stage-conf"
-                                          subject="Rex : Leroy Merlin, Make k8s deployments easy again with Turbine"
+                                    <Talk subject="Rex : Leroy Merlin, Make k8s deployments easy again with Turbine"
                                           speaker="Guillaume Desmidt & Nicolas Lasalle"
                                           company="@Leroy Merlin France & @Zenika"
                                           duration="45min" room="Salle 3" category="REX"/>
                                 </tr>
                                 <tr>
-                                    <Slot hour="10:30"/>
+                                    <Slot hour="15:25"/>
                                     <Break colSize="3" text="Pause"/>
                                 </tr>
                                 <tr>
                                     <Slot hour="15:45"/>
-                                    <Talk color="stage-conf"
-                                          subject="Github Actions : enfin des pipelines accessibles aux développeurs"
+                                    <Talk subject="Github Actions : enfin des pipelines accessibles aux développeurs"
                                           speaker="Antoine Meausoone"
                                           company = "@Sfeir"
                                           duration="45min" room="Salle 1" category="DevOps & SRE"/>
-                                    <Talk color="stage-conf"
-                                          subject="La prod de vos applis Serverless"
+                                    <Talk subject="La prod de vos applis Serverless"
                                           speaker="Alexis Moussine-Pouchkine"
                                           company="@Google"
                                           duration="45min" room="Salle 2" category="Cloud Native"/>
-                                    <Talk color="stage-conf"
-                                          subject="Rex : Le modèle Prismea pour lancer sa Fintech sur AWS"
+                                    <Talk subject="Rex : Le modèle Prismea pour lancer sa Fintech sur AWS"
                                           speaker="Samuel Bailly & Clément Vessella"
                                           company="@Ippon"
                                           duration="45min" room="Salle 3" category="REX"/>
-                                    <Talk color="stage-conf"
-                                          subject="Code Quality & SAST sont disponibles dans GitLab Core ... Et alors?"
+                                    <Talk subject="Code Quality & SAST sont disponibles dans GitLab Core ... Et alors?"
                                           speaker="Philippe Charrière"
                                           company="@Gitlab"
                                           duration="45min" room="Salle 4" category="DevOps & SRE"/>
                                 </tr>
                                 <tr>
                                     <Slot hour="16:40"/>
-                                    <Talk color="stage-conf"
-                                          subject="Je fais partie d'une espèce menacée d'extinction"
+                                    <Talk subject="Je fais partie d'une espèce menacée d'extinction"
                                           speaker="Olivier Poncet"
                                           company = "@Suricats Consulting"
                                           duration="45min" room="Salle 1" category="REX"/>
-                                    <Talk color="stage-conf"
-                                          subject="Optimiser son déploiement Spring Boot dans le cloud"
+                                    <Talk subject="Optimiser son déploiement Spring Boot dans le cloud"
                                           speaker="Julien Dubois"
                                           company="@Microsoft"
                                           duration="45min" room="Salle 2" category="DevOps & SRE"/>
-                                    <Talk color="stage-conf"
-                                          subject="Rex : Marketplace ADEO : Avec Gitops, arrête de bricoler ton cluster"
-                                          speaker="Vincent Guardiola & Franck De Graeve"
+                                    <Talk subject="Rex : Marketplace ADEO : Avec Gitops, arrête de bricoler ton cluster"
+                                          speaker="Vincent Guardiola & Franck De Graeve & Maxime Tricqueneaux"
                                           company="@Adeo"
                                           duration="45min" room="Salle 3" category="REX"/>
-                                    <Talk color="stage-conf"
-                                          subject="Observability is More than Logs, Metrics & Traces"
+                                    <Talk subject="Observability is More than Logs, Metrics & Traces"
                                           speaker="Philipp Krenn"
                                           company="@Elastic"
                                           duration="45min" room="Salle 4" category="Observabilité"/>
